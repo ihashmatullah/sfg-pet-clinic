c5a9858e167735ef9bcd9a526c25cd9cbb90d72d
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-09 16:11:25 -0500

    Adding POJOS. Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 /**
  * Created by HashmatN at 3:51 PM on Sep 09, 2021
+ * Closes ticket issue #2
  */
 public class PetType {
   private String name;
