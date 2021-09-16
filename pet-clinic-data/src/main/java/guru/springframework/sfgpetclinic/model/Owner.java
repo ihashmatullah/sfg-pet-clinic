48f23ec3bc3b469e241cf5a99bb54941be965cf9
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-16 17:16:18 -0500

    Added visits, updated Owner with pet relationship. closes #26.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by HashmatN at 3:48 PM on Sep 09, 2021
  */
 public class Owner extends Person {
+  private Set<Pet> pets;
+
+  public Set<Pet> getPets() {
+    return pets;
+  }
+
+  public void setPets(Set<Pet> pets) {
+    this.pets = pets;
+  }
 }
