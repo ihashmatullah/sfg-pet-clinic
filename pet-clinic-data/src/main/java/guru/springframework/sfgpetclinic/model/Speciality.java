1aa7746bd93fb19dac9466213bf4880d6fd87518
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-20 16:45:07 -0500

    Updated Vets with JPA mappings. closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by HashmatN at 10:52 AM on Sep 17, 2021
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+
+  @Column(name = "description")
   private String description;
 
   public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by HashmatN at 3:47 PM on Sep 09, 2021
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+      inverseJoinColumns = @JoinColumn(name = "speciality_id"))
   private Set<Speciality> specialities = new HashSet<>();
 
   public Set<Speciality> getSpecialities() {
