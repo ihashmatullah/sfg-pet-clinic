5a2cd35daef62105813021fb6084074fc6250ff9
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-21 10:39:43 -0500

    Add visit map service. closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by HashmatN at 10:29 AM on Sep 21, 2021
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
